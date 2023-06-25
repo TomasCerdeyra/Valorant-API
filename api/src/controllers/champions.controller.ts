26f4c87eb9db2c6eafc0cf090ee09bb5dbbf0af5
Lucio <luciogastellu.dev@gmail.com>
2023-06-25 07:18:07 -0300

    Conflicto arreglado

--- api/src/controllers/champions.controller.ts
+++ api/src/controllers/champions.controller.ts
@@ -26,6 +26,14 @@ const getItem = async (req: Request, res: Response) => {
     }
 }
 
-const postItems = async () => { }
+const postItems = async (req: Request, res: Response) => {    
+    try {
+        const postItems = await championsClass.postChampion(req.body);
+        res.status(201).json({ 'NewChampion': postItems });
+    } catch (error: any) {
+        res.status(500).json({ error: error._message });   
+        console.log(error);
+    }
+}
 
 export { getItems,getItem, postItems };
