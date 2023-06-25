b2a3b72f96075b414acedc416b8e1f5ed305751c
Tomas <tomascerdeyraimport@gmail.com>
2023-06-25 05:38:31 -0300

    Connect Mongo

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from "express";
+import "dotenv/config"
+import connectMongo from "./config/connectMongo.config";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+connectMongo()
 
 app.get('*', (req: Request, res: Response) => {
     res.status(404).json({ message: 'Ruta no válida' });
---
+++ api/src/config/connectMongo.config.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import "dotenv/config"
+
+const connectMongo = async () => {
+    const uri = <string>process.env.MONGO_URI;
+    try {
+        await mongoose.connect(uri)
+        console.log("Mongo connect");
+        
+    } catch (error) {
+        console.log('Error connect Mongo');
+    }
+}
+
+export default connectMongo
