14968832f87f4e4d5b9016685f1d942d20812371
Lucio <luciogastellu.dev@gmail.com>
2023-06-28 17:08:48 -0300

    feat: Filter agent by agent type added

--- api/src/routes/champions.routes.ts
+++ api/src/routes/champions.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getItems, postItems, getItem } from "../controllers/champions.controller";
+import { getItems, postItems, getItem, getItemsByType } from "../controllers/champions.controller";
 import checkDev from "../middlewares/checkDev.middlewares";
 
 const router = Router();
 
 router.get('/', getItems);
 router.get('/champions/:id', getItem);
+router.get('/champions/agentType/:agentType', getItemsByType);
 
 router.post('/', checkDev, postItems);
 
